83549dddf7fd037fea2e4d0590f62f2ecc379994
reem alsubhi <Reem.Alsubi.med.s@tuwaiq.edu.sa>
2021-11-15 00:07:22 +0300

    Lap Week8

--- lapApi0/ViewController.swift
+++ lapApi0/ViewController.swift
@@ -4,16 +4,48 @@
 //
 //  Created by MAC on 09/04/1443 AH.
 //
+    
+    import UIKit
 
-import UIKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    class ViewController: UIViewController {
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            getData()
+            // Do any additional setup after loading the view.
+        }
+
+    //https://zenquotes.io/api/quotes/author/
+
+        func getData() {
+            var authorURL = URLComponents()
+            authorURL.scheme = "https"
+            authorURL.host = "zenquotes.io"
+            authorURL.path = "/api/quotes/author/"
+            
+            let AutherRequestURL = authorURL.url!
+            let AutherRequest = URLRequest(url: AutherRequestURL)
+            
+            let SessionConfig = URLSessionConfiguration.default
+               SessionConfig.allowsCellularAccess = false
+               SessionConfig.httpShouldSetCookies = false
+               
+               let AutherSession = URLSession(configuration: SessionConfig)
+                   
+           //        Trigger Session Data Task method (Response)
+                   let fetchDataTask = AutherSession.dataTask(with: AutherRequest) {
+                       (data: Data!, response: URLResponse?, err: Error?) in
+                       print(String(data: data, encoding: .utf8))
+                   }
+                   
+                   fetchDataTask.resume()
+        }
+        
+    // Do any additional setup after loading the view.
     }
 
 
-}
 
